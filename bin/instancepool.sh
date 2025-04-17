9e77649eedd10b6a8cc6bc56ae33490d57daa816
unclemac66 <macstephen66@gmail.com>
2025-04-17 18:17:05 +0000

    add instancpool and ocicli scripts to bin/

---
+++ bin/instancepool.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# ------------------ CONFIGURATION ------------------
+ACTION=$1
+PARTITION="cpu"
+POOL_OCID="ocid1.instancepool.oc1.iad.aaaaaaaapnstgtuv3hxotphavknwlzpvxcyqueuuxkijgxwfazku3fqrrbvq"   # Replace with actual instance pool OCID
+START_WAIT_TIME=120     # Adjust based on actual boot time
+# ---------------------------------------------------
+
+if [[ "$ACTION" != "start" && "$ACTION" != "stop" ]]; then
+  echo "Usage: $0 start|stop"
+  exit 1
+fi
+
+echo "[INFO] Performing '$ACTION' operation on Slurm compute partition nodes..."
+
+# Handle stop operation
+if [[ "$ACTION" == "stop" ]]; then
+  echo "[INFO] Stopping $PARTITION instance pool..."
+  oci compute-management instance-pool stop --instance-pool-id "$POOL_OCID" --auth instance_principal
+
+# Mark stopped nodes as drain in slurm
+  echo "[INFO] Marking nodes in Slurm as DRAIN..."
+  sinfo -p $PARTITION -ho %n | xargs -I {} sudo scontrol update NodeName="{}" State=DRAIN Reason="Manually Paused to stop billing"
+  sleep 5
+
+  echo "[INFO] Start operation complete. Nodes are now DRAINED."
+fi
+
+# Handle start operation
+if [[ "$ACTION" == "start" ]]; then
+
+  echo "[INFO] Starting $PARTITION instance pool..."
+  oci compute-management instance-pool start --instance-pool-id "$POOL_OCID" --auth instance_principal
+
+  echo "[INFO] Waiting for instances to start (sleeping $START_WAIT_TIME seconds) ..."
+  sleep $START_WAIT_TIME
+
+# Mark newly started nodes as idle in slurm
+  echo "[INFO] Marking nodes in Slurm as IDLE..."
+  sinfo -p $PARTITION -ho %n | xargs -I {} sudo scontrol update NodeName="{}" State=RESUME
+  sleep 5
+  echo "[INFO] Start operation complete. Nodes are now IDLE."
+
+fi
