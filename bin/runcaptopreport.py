811962e58b4e655106dcce1682a29ab065dbbd68
unclemac66 <macstephen66@gmail.com>
2025-07-09 19:25:08 +0000

    fix captop

--- bin/runcaptopreport.py
+++ bin/runcaptopreport.py
@@ -12,8 +12,9 @@ capacity_topology_id = args.capacity_id
 signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
 computeClient = oci.core.ComputeClient(config={}, signer=signer)
 
-degraded_nodes=[]
+
 count=0
+repair_nodes=[]
 
 try:
     print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
@@ -21,25 +22,34 @@ try:
     response=oci.pagination.list_call_get_all_results(computeClient.list_compute_capacity_topology_compute_bare_metal_hosts, capacity_topology_id)
     state_counts={}
     for node in response.data:
+        shape=node.instance_shape
         status=node.lifecycle_details
         count+=1
         if status == "AVAILABLE" :
-            if node.lifecycle_state == "ACTIVE":
+            if not node.instance_id is None:
                 status="RUNNING"
             else:
                 status="AVAILABLE"
 
-        if status == "DEGRADED":
-            if node.lifecycle_state == "ACTIVE":
+        if status == "UNAVAILABLE" :
+            if not node.instance_id is None:
                 status="RUNNING"
             else:
-                status="UNAVAILABLE"
+                status="IN_REPAIR"
+
+        if status == "DEGRADED":
+            if node.instance_id is None:
+                status="IN_REPAIR"
+            else:
+                status="RUNNING"
+
         if status in state_counts.keys():
             state_counts[status]+=1
         else:
             state_counts[status]=1
     print("State :: "+str(state_counts)+"\n")
-    print("Total ::", count)
+    print("Total ::", count, shape)
+
 
 except Exception as e:
     sys.exit(1)
